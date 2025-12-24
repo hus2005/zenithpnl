a3514a79c1e42ed55d4a98b17c2381688f9f2a32
Divarion-D <danil.dmitrievn@gmail.com>
2025-12-24 17:08:03 +0300

    Fixed warning when cleaning /tmp

--- src/crons/tmp.php
+++ src/crons/tmp.php
@@ -9,18 +9,36 @@ if (posix_getpwuid(posix_geteuid())['name'] == 'xc_vm') {
         CoreUtilities::checkCron($rIdentifier);
         foreach (array(TMP_PATH, CRONS_TMP_PATH, DIVERGENCE_TMP_PATH, FLOOD_TMP_PATH, MINISTRA_TMP_PATH, SIGNALS_TMP_PATH, LOGS_TMP_PATH) as $rTmpPath) {
             foreach (scandir($rTmpPath) as $rFile) {
-                if (!(600 <= time() - filemtime($rTmpPath . $rFile) && stripos($rFile, 'ministra_') === false)) {
-                } else {
-                    unlink($rTmpPath . $rFile);
+                $fullPath = $rTmpPath . '/' . $rFile;
+
+                // Skip . and ..
+                if ($rFile === '.' || $rFile === '..') {
+                    continue;
+                }
+
+                // Delete only files that are older than 600 seconds and do not start with 'ministra_'
+                if (is_file($fullPath) && time() - filemtime($fullPath) >= 600 && stripos($rFile, 'ministra_') === false) {
+                    unlink($fullPath);
                 }
             }
         }
         foreach (scandir(PLAYLIST_PATH) as $rFile) {
-            if (CoreUtilities::$rSettings['cache_playlists'] > time() - filemtime(PLAYLIST_PATH . $rFile)) {
-            } else {
-                unlink(PLAYLIST_PATH . $rFile);
+            $fullPath = rtrim(PLAYLIST_PATH, '/') . '/' . $rFile;
+
+            // Skip . and ..
+            if ($rFile === '.' || $rFile === '..') {
+                continue;
+            }
+
+            // Only files
+            if (is_file($fullPath)) {
+                // Check if the file is older than the cache duration
+                if (CoreUtilities::$rSettings['cache_playlists'] <= time() - filemtime($fullPath)) {
+                    unlink($fullPath);
+                }
             }
         }
+
         clearstatcache();
         @unlink($rIdentifier);
     } else {
